bebd6e9cd76e5174ac25a2e2ae738e35db5bf532
MomenGit <momentadev@gmail.com>
2022-05-13 10:38:27 +0200

    fix: change solutions of mid-term exam

--- mid-term_2025_exam_code/1.c
+++ mid-term_2025_exam_code/1.c
@@ -4,5 +4,5 @@ void main()
 {
     char *str1 = "abcd";
     char str2[] = "abcd";
-    printf("%d %d %d", sizeof(str1), sizeof(str2), sizeof("abcd"));
+    printf("%d %d %d", sizeof(str1), sizeof(str2), sizeof("abcd"))
 }
--- mid-term_2025_exam_code/6.c
+++ mid-term_2025_exam_code/6.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 void main()
 {
-    int *p = (int *)0x1000;
+    int *p = (int *)malloc(sizeof(int *));
+    *p = 1000;
     printf("%x\n", p + 5);
+    printf("%d\n", p + 5);
 }
